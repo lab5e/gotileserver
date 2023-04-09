7b783799925c3dae7c89ca00b45698392f27b011
Ståle Dahl <stalehd@gmail.com>
2023-04-09 02:23:56 +0200

    Simplify example
    No need to have configurable ports since it's just an example.

--- example/main.go
+++ example/main.go
@@ -1,54 +1,23 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"time"
 
-	"github.com/gorilla/handlers"
 	"github.com/lab5e/gotileserver"
 )
 
-const (
-	defaultListenAddr = "0.0.0.0:8080"
-	readTimeout       = 10 * time.Second
-	readHeaderTimeout = 10 * time.Second
-	writeTimeout      = 15 * time.Second
-	idleTimeout       = 20 * time.Second
-)
-
-var (
-	listenAddr string
-)
-
 func main() {
-	flag.StringVar(&listenAddr, "listen", defaultListenAddr, "HTTP listen addr <host>:<port>")
-	flag.Parse()
-
 	mux := http.NewServeMux()
 
 	if err := gotileserver.RegisterHandler(mux, "http://localhost:8080"); err != nil {
 		log.Fatalf("error registering handler: %v", err)
 	}
 
-	// Handler chain
-	handler := handlers.ProxyHeaders(
-		handlers.CombinedLoggingHandler(os.Stdout, mux))
-
-	server := http.Server{
-		Addr:              listenAddr,
-		Handler:           handler,
-		ReadTimeout:       readTimeout,
-		ReadHeaderTimeout: readHeaderTimeout,
-		WriteTimeout:      writeTimeout,
-		IdleTimeout:       idleTimeout,
-	}
-
 	fmt.Println("open http://localhost:8080/map/index.html for demo page")
-	if err := server.ListenAndServe(); err != nil {
+
+	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Printf("error serving: %v", err)
 	}
 }
